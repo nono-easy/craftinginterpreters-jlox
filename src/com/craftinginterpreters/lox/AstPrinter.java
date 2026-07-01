ce91115159b1eb4a86db76072b397c3a9e5f12f3
nono <90310196+nono-easy@users.noreply.github.com>
2026-07-01 16:07:50 +0800

    Expr生成器、Expr、Expr访问者 代码提交

---
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -0,0 +1,54 @@
+package com.craftinginterpreters.lox;
+
+/**
+ * 把 AST 树打印成字符串
+ * Expr 的访问者
+ */
+class AstPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    // 二元表达式
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme,
+                expr.left, expr.right);
+    }
+
+    // 括号表达式
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    // 字面量
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null)
+            return "nil";
+        return expr.value.toString();
+    }
+
+    // 一元表达式
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    // 递归打印
+    // 树：(-123) * (45.67)
+    // 打印：(* (- 123) (group 45.67))
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+}
